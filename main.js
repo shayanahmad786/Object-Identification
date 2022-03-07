61cfd6216f09b3700f6e65aaad5614557842a771
Shayan Ahmad <39764671+uytuh@users.noreply.github.com>
2022-03-07 13:47:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function mobile()
+{
+    window.location="mobile.html";
+}
+function dog_cat()
+{
+    window.location="dog_cat.html";
+}
+function child()
+{
+    window.location="child.html";
+}
